a9bdfab0de19e75928f448eb6d0ea561c4201d41
Jay <83687427+kuyaaj0@users.noreply.github.com>
2025-12-20 14:50:18 +0800

    Refactor score text update logic in PlayState

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2664,28 +2664,62 @@ class PlayState extends MusicBeatState
 
 	public function scoreTxtUpdate()
 	{
-		scoreTxt.text = "NPS: " + nps + " (Max: " + maxNPS + ")";
+    scoreTxt.text = "NPS: " + nps + " (Max: " + maxNPS + ")";
 
-		if (ClientPrefs.data.playOpponent ? !cpuControlled_opponent : !cpuControlled)
-		{
-			scoreTxt.text += " | " + "Score: " + songScore + " | Misses: " + songMisses + " | Accuracy: " + Math.ceil(ratingPercent * 10000) / 100 + '%'
-				+ " | ";
+    if (ClientPrefs.data.playOpponent ? !cpuControlled_opponent : !cpuControlled)
+    {
+        // --- Smooth score value ---
+        var displayScore:Float = songScore;
+        if (ClientPrefs.smoothScore)
+            displayScore = CoolUtil.smoothLerp(displayScore, songScore, 0.15);
 
-			if (ratingName == 'N/A')
-			{
-				scoreTxt.text += 'N/A';
-			}
-			else
-			{
-				scoreTxt.text += '(' + ratingFC + ') ' + ratingName;
-			}
-		}
-	}
+        // --- Build score text ---
+        scoreTxt.text += " | Score: " + CoolUtil.commaSeparate(Math.floor(displayScore))
+            + " | Misses: " + songMisses
+            + " | Accuracy: " + Math.ceil(ratingPercent * 10000) / 100 + '%'
+            + " | ";
+
+        if (ratingName == 'N/A')
+        {
+            scoreTxt.text += 'N/A';
+        }
+        else
+        {
+            scoreTxt.text += '(' + ratingFC + ') ' + ratingName;
+        }
+    }
+}	
 
 	// Health icon updaters
 	public dynamic function updateIconsScale(elapsed:Float)
 	{
-		var mult:Float = FlxMath.lerp(1, iconP1.scale.x, FlxMath.bound((1 - (elapsed * 13 * playbackRate)), 0, 1));
+		var mult:Float = FlxMath.lerp(1, ipublic function scoreTxtUpdate()
+{
+    scoreTxt.text = "NPS: " + nps + " (Max: " + maxNPS + ")";
+
+    if (ClientPrefs.data.playOpponent ? !cpuControlled_opponent : !cpuControlled)
+    {
+        // --- Smooth score value ---
+        var displayScore:Float = songScore;
+        if (ClientPrefs.smoothScore)
+            displayScore = CoolUtil.smoothLerp(displayScore, songScore, 0.15);
+
+        // --- Build score text ---
+        scoreTxt.text += " | Score: " + CoolUtil.commaSeparate(Math.floor(displayScore))
+            + " | Misses: " + songMisses
+            + " | Accuracy: " + Math.ceil(ratingPercent * 10000) / 100 + '%'
+            + " | ";
+
+        if (ratingName == 'N/A')
+        {
+            scoreTxt.text += 'N/A';
+        }
+        else
+        {
+            scoreTxt.text += '(' + ratingFC + ') ' + ratingName;
+        }
+    }
+}conP1.scale.x, FlxMath.bound((1 - (elapsed * 13 * playbackRate)), 0, 1));
 		iconP1.scale.set(mult, mult);
 		iconP1.updateHitbox();
 
